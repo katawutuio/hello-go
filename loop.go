5b4793982fc0b197a0974c680682cc81bda966fb
katawut <katawutu.io@gmail.com>
2024-01-02 11:38:23 +0700

    loop

---
+++ loop.go
@@ -0,0 +1,17 @@
+package main
+// Go only one loop: for
+
+// loop with 3 components
+for i := 0; i < 10; i++ {
+
+}
+
+// a loop with a condition
+for i < 10 {
+
+}
+
+// An infinite loop
+for {
+	
+}
